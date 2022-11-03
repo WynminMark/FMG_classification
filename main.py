aa38a725a518df86790daae5fd92f44cea0be205
WynminMark <651292793@qq.com>
2022-11-03 18:53:10 +0800

    add filenotfounderr exception manage

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ import PZcalibration as PZ
 import pandas as pd
 
 if __name__ == '__main__':
-    file_path = r"D:\LEARNINNNNNNNNNNNNG\ExperimentData\20220621"
-    save_path = r"D:\LEARNINNNNNNNNNNNNG\ExperimentData\20220621\fianl\data.xlsx"
+    file_path = r"D:\LEARNINNNNNNNNNNNNG\ExperimentData\20221102"
+    save_path = r"D:\LEARNINNNNNNNNNNNNG\ExperimentData\20221102\final\data.xlsx"
     data_type = {'C':"C", 'P':"P"}
-    sensor_num = ["001", "002", "003", "004", "005", "006", "007", "008", "009", "010","011", "012", "013", "014", "015"]
+    sensor_num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"]
     test_num = ["-1", "-2", "-3"]
 
     final_data_writter = pd.ExcelWriter(save_path)
     for i in sensor_num:
         for j in test_num:
-            capacitance_file_path = file_path + '\\' + data_type['C'] + i + j + '.xls'
-            pressure_file_path = file_path + '\\' + data_type['P'] + i + j + '.txt'
-            print(capacitance_file_path)
-            temp_data = PZ.Z_P_calibration(capacitance_file_path, pressure_file_path, 1000)
-            temp_data.to_excel(final_data_writter, sheet_name = i + j, index = True)
+            #capacitance_file_path = file_path + '\\' + data_type['C'] + i + j + '.xls'
+            #pressure_file_path = file_path + '\\' + data_type['P'] + i + j + '.txt'
+            capacitance_file_path = file_path + '\\' + i + j + '.xls'
+            pressure_file_path = file_path + '\\' + i + j + '.txt'
+            try:
+                temp_data = PZ.Z_P_calibration(capacitance_file_path, pressure_file_path, 1000)
+                temp_data.to_excel(final_data_writter, sheet_name = i + j, index = True)
+                print(capacitance_file_path)
+            except FileNotFoundError:
+                print("File not found err: ", capacitance_file_path)
+                continue
+
     final_data_writter.save()
     final_data_writter.close()
     # print(file_path + '\\' + data_type[0] + sensor_num[0] + test_num[0])
 
     pass
+
+
+# FileNotFoundError
